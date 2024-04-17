bfd76f1921d30f637760a21af55dfe8a50b5171f
qxmips <qxmips@gmail.com>
2024-04-17 12:37:48 +0300

    add requirement

--- dags/redshift.py
+++ dags/redshift.py
@@ -2,38 +2,37 @@ import os
 from airflow import DAG
 from airflow.providers.amazon.aws.operators.redshift_data import RedshiftDataOperator
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from datetime import datetime
+
 default_args = {
-    "start_date": "2023-03-01",
+    "start_date": datetime(2023, 3, 1),
+    "owner": "airflow"
 }
 
-
 DAG_ID = os.path.basename(__file__).replace('_dag.py', '')
+
 dag = DAG(
-    f'{DAG_ID}',
+    dag_id=DAG_ID,
     default_args=default_args,
-    schedule="0 0 1,15 * *",
+    schedule_interval="0 0 1,15 * *",
     catchup=False
 )
 
-try:
-    # rd = 
-    
-    rd = SQLExecuteQueryOperator(
-        task_id='setup__create_table',
-        conn_id="redshift_default",
-        sql="""
-            CREATE TABLE IF NOT EXISTS fruit (
-            fruit_id INTEGER,
-            name VARCHAR NOT NULL,
-            color VARCHAR NOT NULL
-            );
-        """,
-    )
+# Task 1 using SQLExecuteQueryOperator
+task1_sql_exec_query = SQLExecuteQueryOperator(
+    task_id='execute_query_using_sql_operator',
+    sql="SELECT usename, usesysid, usesuper FROM pg_user WHERE usename=current_user;",
+    conn_id="redshift_default",  # Make sure to configure this connection in Airflow
+    dag=dag
+)
 
-    rd
-    
-except Exception as msg:
-    print(msg)
-    
+# Task 2 using RedshiftDataOperator
+task2_redshift_data = RedshiftDataOperator(
+    task_id='execute_query_using_redshift_operator',
+    sql="SELECT usename, usesysid, usesuper FROM pg_user WHERE usename=current_user;",
+    database="dev",
+    dag=dag
+)
 
-    #https://blog.beachgeek.co.uk/using-redshift-with-mwaa/
+# Define task order
+task1_sql_exec_query >> task2_redshift_data
