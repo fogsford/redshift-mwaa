189fef4b5ea5eebbdcea0fce4dc8e889e34c1a7b
qxmips <qxmips@gmail.com>
2024-04-17 11:47:46 +0300

    add requirement

--- dags/redshift.py
+++ dags/redshift.py
@@ -2,6 +2,7 @@ import os
 from airflow import DAG
 from airflow.providers.amazon.aws.operators.redshift_data import RedshiftDataOperator
 from airflow.providers.amazon.aws.operators.redshift_sql import RedshiftSQLOperator
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 default_args = {
     "start_date": "2023-03-01",
 }
@@ -20,6 +21,7 @@ try:
     
     rd = RedshiftSQLOperator(
         task_id='setup__create_table',
+        redshift_conn_id="redshift_default",
         sql="""
             CREATE TABLE IF NOT EXISTS fruit (
             fruit_id INTEGER,
